c5b83b5599eabdd6769d41aec77b2e1365867668
Richie Lee <richie@bzzzt.io>
2020-04-17 08:36:22 +0100

    fun with strings

---
+++ python_work/ada_lovelace.py
@@ -0,0 +1,26 @@
+name = "ada lovelace"
+print(name.title())
+
+print(name.lower())
+
+print(name.upper())
+
+first_name = "ada"
+
+last_name = "lovelace"
+
+print("3.6")
+#3.6
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("3.5")
+#3.5
+full_name = "{} {}".format(first_name, last_name)
+print(full_name)
+print("Hello, {}!".format(full_name.title()))
+message = "Hello, {}!".format(full_name.title())
+print(message)
