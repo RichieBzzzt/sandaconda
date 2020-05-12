58161ff325a90a0aa6acd71ee3118e2fbee8f47e
Richie Lee <richie@bzzzt.io>
2020-05-12 17:07:37 +0100

    tuple changes

---
+++ python_work/tuple_work.py
@@ -0,0 +1,25 @@
+dimensions = (200, 50)
+
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+
+print("\nOriginal dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (250, 150)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+menu_items = ('ham', 'cheese', 'crisps', 'olives', 'dip')
+for menu_item in menu_items:
+    print(menu_item)
+
+
+menu_items = ('pizza', 'chicken', 'crisps', 'olives', 'dip')
+for menu_item in menu_items:
+    print(menu_item)
