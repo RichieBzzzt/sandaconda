f9fa2b23548fffefaa76352c80a702be5e18172f
Richie Lee <richie@bzzzt.io>
2020-10-19 14:03:02 +0100

    finish functions

--- pizza.py
+++ pizza.py
@@ -7,3 +7,16 @@ def make_pizza(*toppings):
 make_pizza('pepperoni')
 
 make_pizza('mushrooms', 'chicken', 'anchovies')
+
+
+def build_pizza(first_topping, second_topping, **toppings):
+    toppings['first_topping'] = first_topping
+    toppings['second_topping'] = second_topping
+    return toppings
+
+
+toppings = build_pizza('cheese', 'tomato sauce',
+                       third_topping='olives',
+                       fourth_topping='pepperoni')
+
+print(toppings)
